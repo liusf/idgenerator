ac53d50d78ea39c00129e6faa9d8cf4bcb9d6150
LIU Shufa <shufa@tigerbrokers.com>
2016-01-12 17:14:57 +0800

    check peers with consul(not register service)

---
+++ client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/liusf/idgenerator/gen-go/idgenerator"
+)
+
+func newIdGeneratorClient(host string, port int) (timestamp int64, workerId int64) {
+	trans, err := thrift.NewTSocket(net.JoinHostPort(host, fmt.Sprint(port)))
+	if err != nil {
+		fmt.Printf("Error resolving address %s:%d, %v\n", host, port, err)
+		os.Exit(1)
+	}
+	protocolFactory := thrift.NewTCompactProtocolFactory()
+	client := idgenerator.NewIdGeneratorClientFactory(trans, protocolFactory)
+	if err := trans.Open(); err != nil {
+		fmt.Println("Error opening socket to 2", host, ":", port, " ", err)
+		os.Exit(1)
+	}
+	timestamp, err1 := client.GetTimestamp()
+	peerDatacenterId, err2 := client.GetDatacenterId()
+	if err1 != nil || err2 != nil {
+		fmt.Printf("Could not talk to peer %s:%d\n", host, port)
+		os.Exit(1)
+	}
+	defer trans.Close()
+	return timestamp, peerDatacenterId
+}
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
 	"os"
+	"strings"
 
 	"git.apache.org/thrift.git/lib/go/thrift"
 	"github.com/liusf/idgenerator/gen-go/idgenerator"
@@ -21,10 +26,17 @@ func main() {
 	help := flag.Bool("h", false, "show this help info")
 	workerId := flag.Int("w", 0, "worker id (0-31)")
 	datacenterId := flag.Int("dc", 0, "data center id (0-7)")
+	consulServers := flag.String("consul", "", "check peers with consul server hosts(ip:port,ip:port,...)")
+
 	flag.Parse()
 	if *port <= 0 || *help {
 		Usage()
 		os.Exit(1)
+	}
+
+	if *consulServers != "" {
+		sanityCheck(int64(*workerId), int64(*datacenterId), *consulServers)
+		fmt.Println("Sanity check OK")
 	}
 
 	protocolFactory := thrift.NewTCompactProtocolFactory()
@@ -48,4 +60,66 @@ func main() {
 	} else {
 		fmt.Println("running id generator server")
 	}
+}
+
+type ServiceAddr struct {
+	ServiceAddress string
+	ServicePort    int
+}
+
+func sanityCheck(workerId int64, datacenterId int64, consulServers string) {
+	// check peers, not duplicated datacentId & workerId, not too much time shift
+	addrs := getPeerAddrs(consulServers)
+	if addrs == nil {
+		fmt.Println("Unable to resolve peers address", consulServers)
+		os.Exit(1)
+	}
+	if len(addrs) == 0 {
+		fmt.Println("No peers")
+		return
+	}
+	var sumTimestamp int64
+	for _, addr := range addrs {
+		timestamp, peerDatacenterId := newIdGeneratorClient(addr.ServiceAddress, addr.ServicePort)
+		if datacenterId != peerDatacenterId {
+			fmt.Printf("Worker at %s has datacenter_id %d, but ours is %d", addr, peerDatacenterId, datacenterId)
+			os.Exit(1)
+		} else {
+			sumTimestamp += timestamp
+		}
+	}
+	avg := sumTimestamp / int64(len(addrs))
+	if math.Abs(float64(avg-getTimestamp())) > 10000.0 {
+		fmt.Printf("Timestamp sanity check failed. Mean timestamp is %d, but mine is %d, "+
+			"so I'm more than 10s away from the mean", avg, getTimestamp())
+		os.Exit(1)
+	}
+}
+
+func getPeerAddrs(consulServers string) []ServiceAddr {
+	servers := strings.Split(consulServers, ",")
+	var services []ServiceAddr
+	for _, server := range servers {
+		serviceUrl := fmt.Sprintf("http://%s/v1/catalog/service/idgenerator", server)
+		resp, err := http.Get(serviceUrl)
+		if err != nil {
+			fmt.Println("get peers address error ", serviceUrl, err)
+			continue
+		} else {
+			bytes, err := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+			if err != nil {
+				fmt.Println("get service content error ", serviceUrl, err)
+				continue
+			} else {
+				if err := json.Unmarshal(bytes, &services); err != nil {
+					fmt.Println("service definition parse error", err)
+					continue
+				} else {
+					return services
+				}
+			}
+		}
+	}
+	return nil
 }
